27c25abaf115c0d3f11991dea4c3574b1a9d11cd
Dagemawi Alemayehu <86920356+DagemawiDeveloper@users.noreply.github.com>
2026-08-24 04:52:28 +0300

    refactor: align product fields and casts with checkout data

--- app/Models/Shop/Product.php
+++ app/Models/Shop/Product.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-        protected $fillable = [
+
+    protected $fillable = [
         'product_name',
         'product_catagories',
         'product_description',
@@ -17,6 +18,7 @@ class Product extends Model
         'product_list_by',
         'product_price',
         'product_discount',
+        'product_logical_price',
         'product_brand',
         'product_quantity',
         'product_available',
@@ -24,13 +26,26 @@ class Product extends Model
         'plan_id',
         'is_deal',
         'is_featured',
-        // 'deal_target',
+        'deal_name',
+        'deal_target',
+        'deal_index',
         'main_image',
+        'banner_image',
         'big_image',
         'big_image1',
         'big_image2',
-
-        
     ];
 
+    protected $casts = [
+        'product_price' => 'decimal:2',
+        'product_discount' => 'decimal:2',
+        'product_logical_price' => 'decimal:2',
+        'product_quantity' => 'integer',
+        'product_available' => 'boolean',
+        'product_trending' => 'boolean',
+        'is_deal' => 'boolean',
+        'is_featured' => 'boolean',
+        'deal_index' => 'boolean',
+        'deal_target' => 'date',
+    ];
 }
